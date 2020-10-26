053fcde989b0d31b952b0e640be4077d2d5abdbc
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-10-26 12:38:21 +0400

    Add files via upload

---
+++ ClassLibrary/ClassLibrary/Notification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Notification
+    {
+        internal string Title { get; set; }
+        protected internal string Description { get; set; }
+        private protected DateTime Time { get; set; }
+    }
+
+    internal class Email: Notification
+    {
+        //Notification notification = new Notification();
+        public Email()
+        {
+            Description = "Email body";
+            Console.WriteLine(Description);
+            Time = new DateTime();
+            Console.WriteLine(Time);
+        }
+    }
+
+    class Test
+    {
+        public Test()
+        {
+            //Title = "Bashliq";
+            //Console.WriteLine(Title);
+            Notification notification = new Notification();
+            //notification.Title = "Bashliq";
+            //Console.WriteLine(notification.Title);
+            //notification.Description = "Hello World";
+            //Console.WriteLine(notification.Description);
+        }
+    }
+}
---
+++ ModifiersSecond/ModifiersSecond/Extention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiersSecond
+{
+    //static class - all members must be static!!! Cann't instance!!! Cann't get inheretance!!! 
+    static class Extention
+    {
+        static Extention()
+        {
+            Test();
+        }
+        public static int MyProperty { get; set; }
+
+        private static void Test()
+        {
+            Console.WriteLine("Nahide istedi");
+        }
+    }
+
+}
